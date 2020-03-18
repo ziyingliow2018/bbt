7bc51369efb93d459068fefa438595ff86c3217c
Khoo Xian Shuang <54575114+xskhoo@users.noreply.github.com>
2020-03-18 14:29:09 +0800

    Update notification.py

--- notification.py
+++ notification.py
@@ -1,59 +1,51 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from sqlalchemy import Date
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/book'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/bubbletea'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-
-class Book(db.Model):
-    __tablename__ = 'book'
-
-    isbn13 = db.Column(db.String(13), primary_key=True)
-    title = db.Column(db.String(64), nullable=False)
-    price = db.Column(db.Float(precision=2), nullable=False)
-    availability = db.Column(db.Integer)
-
-    def __init__(self, isbn13, title, price, availability):
-        self.isbn13 = isbn13
-        self.title = title
-        self.price = price
-        self.availability = availability
-
+CORS(app)
+ 
+class order(db.Model):
+    __tablename__ = 'order'
+ 
+    orderid = db.Column(db.String(13), primary_key=True)
+    base = db.Column(db.String(100), nullable=False)
+    datetime = db.Column(db.Date, nullable=False)
+    toppings = db.Column(db.String(200), nullable=False)
+    totalprice = db.Column(db.Float(precision=2), nullable=False)
+    status = db.Column(db.String(15), nullable=False)
+ 
+    def __init__(self, orderid, base, datetime, toppings, totalprice, status):
+        self.orderid = orderid
+        self.base = base
+        self.datetime = datetime
+        self.toppings = toppings
+        self.totalprice = totalprice
+        self.status = status
+ 
     def json(self):
-        return {"isbn13": self.isbn13, "title": self.title, "price": self.price, "availability": self.availability}
-
-
-@app.route("/book")
+        return {"orderid": self.orderid, "base": self.base, "datetime": self.datetime, 
+        "toppings": self.toppings,"totalprice": self.totalprice, "status": self.status}
+ 
+ 
+@app.route("/order")
 def get_all():
-    return jsonify({"books": [book.json() for book in Book.query.all()]})
+    return jsonify({"orders": [order.json() for order in order.query.all()]})
+ 
+ 
+@app.route("/order/<string:orderid>")
+def find_by_orderid(orderid):
+    single_order = order.query.filter_by(orderid=orderid).first()
+    if order:
+        return jsonify(single_order.json())
+    return jsonify({"message": "Order not found."}), 404
 
-
-@app.route("/book/<string:isbn13>")
-def find_by_isbn13(isbn13):
-    book = Book.query.filter_by(isbn13=isbn13).first()
-    if book:
-        return jsonify(book.json())
-    return jsonify({"message": "Book not found."}), 404
-
-
-@app.route("/book/<string:isbn13>", methods=['POST'])
-def create_book(isbn13):
-    if (Book.query.filter_by(isbn13=isbn13).first()):
-        return jsonify({"message": "A book with isbn13 '{}' already exists.".format(isbn13)}), 400
-
-    data = request.get_json()
-    book = Book(isbn13, **data)
-
-    try:
-        db.session.add(book)
-        db.session.commit()
-    except:
-        return jsonify({"message": "An error occurred creating the book."}), 500
-
-    return jsonify(book.json()), 201
-
+//send to notification API
+ 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
