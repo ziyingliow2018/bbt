1c93cf6ee7349dd3aa9fc192d1f763719e4efe45
liewhuiyun <54573060+liewhuiyun@users.noreply.github.com>
2020-03-30 21:14:42 +0800

    fb api

--- login.php
+++ login.php
@@ -43,10 +43,57 @@
                 </div>
 
                 <div class="form-item">
-                    <input type="button" class="login facebook" value='Connect with facebook'>
-                    <br>
-                    
-                    <div class="clear-fix"></div>
+                <?php
+                    require ("assets/vendor/vendor/autoload.php");
+                    session_start();
+
+                    if(isset($_GET['state'])) {
+                        $_SESSION['FBRLH_state'] = $_GET['state'];
+                    }
+
+                    /*Step 1: Enter Credentials*/
+                    $fb = new \Facebook\Facebook([
+                        'app_id' => '134169310679094',
+                        'app_secret' => '264b5647eabf27806a5122d127df8dc7',
+                        'default_graph_version' => 'v2.12',
+                        //'default_access_token' => '{access-token}', // optional
+                    ]);
+
+
+                    /*Step 2 Create the url*/
+                    if(empty($access_token)) {
+
+                        echo "<a href='{$fb->getRedirectLoginHelper()->getLoginUrl("http://localhost/bbt/index.html")}'>Login with Facebook </a>";
+                    }
+
+                    /*Step 3 : Get Access Token*/
+                    $access_token = $fb->getRedirectLoginHelper()->getAccessToken();
+
+
+                    /*Step 4: Get the graph user*/
+                    if(isset($access_token)) {
+
+
+                        try {
+                            $response = $fb->get('/me',$access_token);
+
+                            $fb_user = $response->getGraphUser();
+
+                            echo  $fb_user->getName();
+
+
+
+
+                            //  var_dump($fb_user);
+                        } catch (\Facebook\Exceptions\FacebookResponseException $e) {
+                            echo  'Graph returned an error: ' . $e->getMessage();
+                        } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+                            // When validation fails or other local issues
+                            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+                        }
+
+                    }
+                    ?>
                 </div>
             </div>
         </div>
